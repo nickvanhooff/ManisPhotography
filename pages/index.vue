21cf59b89354e460a3540f0fef6c369c3dc94531
nickvanhooff <nickvanhooff03@gmail.com>
2025-09-06 01:22:41 +0200

    Add Netlify Identity widget for authentication and update Decap CMS configuration in admin files

--- pages/index.vue
+++ pages/index.vue
@@ -29,4 +29,23 @@
 </template>
 
 <script setup lang="ts">
+// Add Netlify Identity widget for Decap CMS authentication
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+
+// Handle Netlify Identity login redirect
+onMounted(() => {
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", (user) => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+});
 </script> 
